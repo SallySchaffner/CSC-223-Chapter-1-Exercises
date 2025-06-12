d38cff5af42821e2a148c22eb255b21643184d17
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2025-06-12 23:36:48 +0000

    Program has the function and the comprehension from R-1.6

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello World!')
+def sumOfSquares(n):
+  sum = 0
+  for i in range(1, n):
+    sum += i**2
+  return sum
+
+def main():
+  n = int(input("Enter a positive integer: "))
+  while (n <=0):
+    n = int(input("Integer must be positive\nEnter a positive integer: "))
+  sumSquares = sumOfSquares(n)
+  print("Sum of squares from function: ", sumSquares)
+
+  sumSquares2 = sum([i*i for i in range(1, n)])
+  print("Sum of squares from comprehension: ", sumSquares2)
+
+main()

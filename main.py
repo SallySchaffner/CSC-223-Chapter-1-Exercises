ca3511b7348d790ea4952d4d3113577f8c5d479c
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2025-06-12 02:13:16 +0000

    Completed is_even using repeated subtractions

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello World!')
+def is_even(k):
+  if k < 0:
+    k *= -1
+  while k > 0:
+    k -= 2
+  return not(bool(k))
+
+def main():
+  cont = input("Enter an integer: ")
+  while cont != "q":
+    n = int(cont)
+    if (is_even(n)):
+      print("This number is even")
+    else:
+      print("This number is odd")
+    cont = input("Enter an integer or type q to quit: ")
+
+main()

72cf99e72b9680edb34d89dbea526bb0692240f6
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2025-06-12 21:38:53 +0000

    Simplified the print statements

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+"""
+  Test driver for getSequence and minmax functions
+"""
 def minmax(numSeq):
+  """
+    Given an input sequence of integers, determines the min and max values in the sequence.
+
+    Precondition:
+      Do not use Pythons min() and max() built-in functions
+
+    Parameter:
+      numSeq, a list containing the sequence of numbers to analyze.
+
+    Returns:
+      A tuple containing the minimum and maximum values in the sequence
+  """
+  # list items are accessed using square brackets [].
   min = numSeq[0]
   max = numSeq[0]
   for val in numSeq:
@@ -6,24 +22,39 @@ def minmax(numSeq):
       min = val
     if val > max:
       max = val
+  # Tuple is created using parentheses ()
   return (min, max)
 
+"""
+  Gets a sequence of numbers from keyboard input and stores them in a list
+
+  Parameters:
+    None
+
+  Returns:
+    A list containing the numbers entered by the user
+"""
 def getSequence():
-  seq = []
+  seq = []    # Start with an empty sequence
   cont = "go"
   val = int(input("Enter an integer: "))
   while cont != "q":
-    seq.append(val)
+    seq.append(val)   # the append method adds a value to the end of the list
+                      # (and increases the size of the list)
     cont = input("Enter an integer or enter q to quit: ")
     if cont != 'q':
       val = int(cont)
   return seq
 
+"""
+  Test driver for getSequence and minmax
+"""
 def main():
-  newSeq = getSequence()
-  minMax = minmax(newSeq)
-  print ("The sequence min is " + str(minMax[0]))
-  print ("The sequence max is: "+ str(minMax[1]))
+  newSeq = getSequence()  # The list is created inside the function and returned
+  minMax = minmax(newSeq) # This function returns a tuple
+  # Access the individual items of the tuple using indexes
+  print ("The sequence min is:", minMax[0])
+  print ("The sequence max is:", minMax[1])
 
 main()
   

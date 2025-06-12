1def053d2278cc4840fbea15eb2bc8379b5e0a36
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2025-06-12 20:55:38 +0000

    Added minmax program (R-1.3)

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print('Hello World!')
+def minmax(numSeq):
+  min = numSeq[0]
+  max = numSeq[0]
+  for val in numSeq:
+    if val < min:
+      min = val
+    if val > max:
+      max = val
+  return (min, max)
+
+def getSequence():
+  seq = []
+  cont = "go"
+  val = int(input("Enter an integer: "))
+  while cont != "q":
+    seq.append(val)
+    cont = input("Enter an integer or enter q to quit: ")
+    if cont != 'q':
+      val = int(cont)
+  return seq
+
+def main():
+  newSeq = getSequence()
+  minMax = minmax(newSeq)
+  print ("The sequence min is " + str(minMax[0]))
+  print ("The sequence max is: "+ str(minMax[1]))
+
+main()
+  
+  

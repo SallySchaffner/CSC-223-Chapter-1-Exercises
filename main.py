fcffac3f77bb3d460d184d2af43db433a5a841be
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2025-06-12 02:09:35 +0000

    Completed function is_multiple()

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('Hello World!')
+def is_multiple(n, m):
+ return (n % m == 0)
+
+def main():
+  cont = "go"
+  while (cont != "q"):
+    n = int(input("Enter an integer: "))
+    m = int(input("Enter another integer: "))
+    if is_multiple(n, m):
+      print(str(m) + " is a multiple of " + str(n))
+    else:
+      print(str(m) + " is not a multiple of " + str(n))
+    cont = input("Hit enter to continue or q to quit ")
+
+
+main()

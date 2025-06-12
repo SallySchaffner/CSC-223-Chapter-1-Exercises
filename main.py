bc54356a71d1ec0339ff7f5b71fffaee6723e020
Sally Schaffner <151073463+SallySchaffner@users.noreply.github.com>
2025-06-12 23:54:35 +0000

    Sum of odd squares via function (R-1.5) and comprehension(R-1.6)

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 
-print("Hello World!")
+def sumOddSquares(n):
+  sum = 0
+  for i in range(1, n):
+    if i % 2 == 1:
+      sum += i*i
+      print(i, sum)
+  return sum
+
+def main():
+  n = int(input("Enter a positive integer: "))
+  while n <= 0:
+    n = int(input("Integer must be positive\nEnter a positive integer: "))
+  sumOddSq = sumOddSquares(n)
+  print("Sum odd squares from the function: ", sumOddSq)
+
+  sumOddSq_C = sum(i*i for i in range(1, n) if i % 2 == 1 )
+  print("Sum of odd squares from comprehension: ", sumOddSq_C)
+
+main()
+  
+    
